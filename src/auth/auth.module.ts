c9926ae2c5d247986969c3e79a2cce54cd3e45cc
afaq1182 <74967960+afaq1182@users.noreply.github.com>
2021-09-16 02:05:20 +0500

    Add files via upload

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,10 +6,11 @@ import { LocalStrategy } from './local.strategy';
 import { SessionSerializer } from './session.serializer';
 import { AuthController } from '../Controllers/auth.controller';
 import { UserRepository } from 'src/Repositories/User-Respository';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersService, PassportModule.register({session: true})],
-  providers: [AuthService ,UsersService , LocalStrategy, SessionSerializer, UserRepository],
+  imports: [UsersService, PassportModule.register({ session: true }), TypeOrmModule.forFeature(UserRepository)],
+  providers: [AuthService, UsersService, LocalStrategy, SessionSerializer, UserRepository],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
