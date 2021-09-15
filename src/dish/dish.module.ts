9b35dcedcb0d47abad5eb63990d9832374bb1966
afaq1182 <74967960+afaq1182@users.noreply.github.com>
2021-09-16 02:11:51 +0500

    Add files via upload

--- src/dish/dish.module.ts
+++ src/dish/dish.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { DishController } from './dish.controller';
+import { DishController } from '../controllers/dish.controller';
 import { DishService } from '../Services/dish.service'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DishRepository } from '../Repositories/Dish-Repository';
 @Module({
+  imports: [TypeOrmModule.forFeature([DishRepository])],
   controllers: [DishController],
   providers: [DishService]
 })
